7efae14405efe377b9c162b9c8c78b35f1379eac
Juto Yu <jutkko@gmail.com>
2017-10-07 12:23:41 +0100

    Rename file

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 func main() {
 	ctx := context.Background()
 
+	// TODO: Pass the path to this file and let the binary initialise the config
 	b, err := ioutil.ReadFile("client_secret.json")
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %v", err)
@@ -63,7 +64,6 @@ func main() {
 	} else {
 		fmt.Printf("No upcoming events found.\n")
 	}
-
 }
 
 // getClient uses a Context and Config to retrieve a Token
@@ -73,31 +73,13 @@ func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
 	if err != nil {
 		log.Fatalf("Unable to get path to cached credential file. %v", err)
 	}
+
 	tok, err := tokenFromFile(cacheFile)
 	if err != nil {
 		tok = getTokenFromWeb(config)
 		saveToken(cacheFile, tok)
 	}
 	return config.Client(ctx, tok)
-}
-
-// getTokenFromWeb uses Config to request a Token.
-// It returns the retrieved Token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		log.Fatalf("Unable to read authorization code %v", err)
-	}
-
-	tok, err := config.Exchange(oauth2.NoContext, code)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web %v", err)
-	}
-	return tok
 }
 
 // tokenCacheFile generates credential file path/filename.
@@ -123,6 +105,25 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 	err = json.NewDecoder(f).Decode(t)
 	defer f.Close()
 	return t, err
+}
+
+// getTokenFromWeb uses Config to request a Token.
+// It returns the retrieved Token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
 }
 
 // saveToken uses a file path to create a file and store the
